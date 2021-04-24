096d7c1324fe55c597f1c6a7e1ea64a3347508c7
Pattarawan-012 <pattarawan2001@gmail.com>
2021-04-24 16:05:26 +0700

    ข้อ 7

---
+++ ข้อสอบกลางภาค ข้อ 7.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mid_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ทำไมได้จริงๆค่ะ ขอโทษนะคะ โง่ไม่ไหวเเล้ว จุ๊บุ 
+        }
+    }
+}
